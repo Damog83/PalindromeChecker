3af102184fd0ccabfb03b70b4654032755605366
Damien Gittings <damiengittings@hotmail.co.uk>
2023-05-03 22:52:25 +0100

    Palindrome tests and katas completed

--- src/main/java/com/sparta/dg/PalindromeChecker.java
+++ src/main/java/com/sparta/dg/PalindromeChecker.java
@@ -4,12 +4,36 @@ public class PalindromeChecker {
 
     public static boolean isPalindrome(String word) {
         if(word.length() < 3) return false;
-
         StringBuilder sb = new StringBuilder(word);
         String reversed = sb.reverse().toString();
-
         return word.equals(reversed);
     }
+
+    public static boolean isPalindromeLoop(String word) {
+        if(word.length() < 3) return false;
+        int middle = word.length() / 2;
+        for(int i = 0; i < middle; i++) {
+            if(word.charAt(i) != word.charAt(word.length() - 1 - i)) return false;
+        }
+        return true;
+    }
+
+    public static String longestPalindrome(String[] words) {
+
+        int length = 0;
+        String palindrome = null;
+
+        for(String word : words) {
+            if(isPalindrome(word) && word.length() > length){
+                palindrome = word;
+                length = word.length();
+            } else if (isPalindrome(word) && word.length() == length) {
+                palindrome += " and " + word;
+            }
+        }
+        return (palindrome == null) ? "No Palindromes" : palindrome;
+    }
     public static void main(String[] args) {
+
     }
 }
--- src/test/java/com/sparta/dg/PalindromeCheckerTests.java
+++ src/test/java/com/sparta/dg/PalindromeCheckerTests.java
@@ -11,19 +11,66 @@ public class PalindromeCheckerTests {
     @Test
     @DisplayName("When word is less than 3 letters return false")
     void checkForWordLengthUnder3() {
-        Assertions.assertEquals(false, PalindromeChecker.isPalindrome("No"));
+        Assertions.assertFalse(PalindromeChecker.isPalindrome("No"));
     }
 
     @Test
     @DisplayName("When a word passed is not a palindrome returns false")
     void checkNonPalindrome() {
-        Assertions.assertEquals(false, PalindromeChecker.isPalindrome("Not"));
+        Assertions.assertFalse(PalindromeChecker.isPalindrome("Not"));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"pop", "deed", "kayak", "testset"})
-    @DisplayName("When a palindrome is passed returnds true")
+    @DisplayName("When a palindrome is passed returns true")
     void checkPalindrome(String word) {
-        Assertions.assertEquals(true, PalindromeChecker.isPalindrome(word));
+        Assertions.assertTrue(PalindromeChecker.isPalindrome(word));
+    }
+
+    @Test
+    @DisplayName("When word is less than 3 letters return false")
+    void checkWordLengthUnder3WithLoop() {
+        Assertions.assertFalse(PalindromeChecker.isPalindromeLoop("No"));
+    }
+
+    @Test
+    @DisplayName("When a word passed is not a palindrome returns false")
+    void checkNonPalindromeWithLoop() {
+        Assertions.assertFalse(PalindromeChecker.isPalindromeLoop("Parrot"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pop", "deed", "kayak", "testset"})
+    @DisplayName("When a palindrome is passed returns true")
+    void checkPalindromeWithLoop(String word) {
+        Assertions.assertTrue(PalindromeChecker.isPalindromeLoop(word));
+    }
+
+    @Test
+    @DisplayName("When no palindromes are present in the sentence returns no palindromes")
+    void checkSentenceWithNoPalindrome() {
+        String[] sentence = {"I", "have", "no", "palindromes"};
+        Assertions.assertEquals("No Palindromes",PalindromeChecker.longestPalindrome(sentence));
+    }
+
+    @Test
+    @DisplayName("When a single palindrome is present in the sentence returns palindrome")
+    void checkSentenceWithSinglePalindrome() {
+        String[] sentence = {"I", "have", "a", "kayak"};
+        Assertions.assertEquals("kayak",PalindromeChecker.longestPalindrome(sentence));
+    }
+    //tests with longest word in different order
+    @Test
+    @DisplayName("When a sentence contains more than one palindrome of differing lengths the largest is returned")
+    void checkSentenceWithTwoPalindromesOfSameLength() {
+        String[] sentence = {"What", "are", "the", "stats", "on", "that", "racecar", "?"};
+        Assertions.assertEquals("racecar", PalindromeChecker.longestPalindrome(sentence));
+    }
+
+    @Test
+    @DisplayName("When more than one palindrome have the lonest length")
+    void checkSentenceForMoreThanOnePalindromeOfTheLongestLength() {
+        String[] sentence = {"bob", "cat", "kayak", "radar", "noon", "pop"};
+        Assertions.assertEquals("kayak and radar", PalindromeChecker.longestPalindrome(sentence));
     }
 }
